feb429963d7a4bd50ecf773ec75af07ef55e3e51
Jerrod <kh_duster@charter.net>
2017-06-01 13:01:45 -0500

    Add project files.

---
+++ EntityTest/Helpers/EntityHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.Objects;
+using JSON = Newtonsoft.Json.JsonConvert;
+namespace EntityTest.Helpers
+{
+    class EntityHelper
+    {
+    }
+
+    public static class EntityExtensions
+    {
+        public static T ToType<T>(this ObjectResult results)
+        {
+            ObjectResult r = results;
+            return JSON.DeserializeObject<T>(JSON.SerializeObject(r));
+        }
+    }
+}
---
+++ EntityTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.Objects;
+using EntityTest.Helpers;
+
+namespace EntityTest
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            using (TestPeopleEntities db = new TestPeopleEntities())
+            {
+                Console.WriteLine("Get People Results:");
+                List<Person> people = db.GetPeople().ToType<List<Person>>();
+                foreach (var p in people)
+                {
+                    Console.WriteLine(p.FirstName + " " + p.LastName);
+                }
+                Console.WriteLine("");
+
+                Console.WriteLine("Bet By ID:");
+                List<Person> p1 = db.GetPerson(3).ToType<List<Person>>();
+                Console.WriteLine(p1[0].LastName + " " + p1[0].FirstName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
